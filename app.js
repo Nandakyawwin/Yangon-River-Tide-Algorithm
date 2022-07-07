5f1a7f325d05d5bf3a908c6a39eb80389b282d15
Nandakyawwin <nandakyawwin065480@gmail.com>
2022-07-07 13:32:53 +0630

    add into to package.json for publishing to npm

--- app.js
+++ app.js
@@ -1,42 +1,90 @@
-let ppt = prompt(`ဒီနေ့မြန်မာလိုဘယ်နှရက်နေ့ပါလဲ?`);
-let errorMsg = `Myanmar Date valid number are 1 to 15. မြန်မာပြက္ခဒိန်တွင် ရက်သည် ၁ မှ ၁၅ အထိသာရှိပါသည်။ မိမိထည့်သွင်းသောရက်မှာ ${ppt} ဖြစ်နေပါသည်။`;
-let errorApiMsg = document.querySelector('.errMSG');
-function Api(){
-if(ppt == 0 || ppt > 15){
-    alert(errorMsg);
-    location.reload();
-}else{
-    doApi();
-}
-}
-function doApi(){
+
+var prompt = require('prompt');
+
+  prompt.start();
+
+  prompt.get(['mmdate'], function (err, result) {
+    
+     let errorMsg = `Myanmar Date valid number are 1 to 15. မြန်မာပြက္ခဒိန်တွင် ရက်သည် ၁ မှ ၁၅ အထိသာရှိပါသည်။ မိမိထည့်သွင်းသောရက်မှာ  ဖြစ်နေပါသည်။`;
+
+    // let value = result.mmdate;
+
+    // console.log( result.mmdate);
+     let value = result.mmdate;
+    //  console.log( value);
+     if(value.type ==! Number){
+         console.log(errorMsg);
+     }else{
+     console.log('you typed ' + value);
+
+     }
+// let ppt = value;
+
+// console.log(ppt);
+
+// function Api()
+
+// {
+
+// if(ppt == 0 || ppt > 15)
+
+// {
+
+//     console.log(errorMsg);
+
+// }
+// else
+// {
+
+//     doApi();
+
+// }
+
+// }
+
+function doApi()
+
+{
+
     let getMmDay = ppt;
+
     let getday = getMmDay * 100; 
+
     let hourbytide = getMmDay * 25; 
+
     let result = getday - hourbytide; 
+
     let tidebyhour = result /100; 
+
     let Uphour = Math.floor(tidebyhour);
+
     let tidebyminute = result - Uphour * 100;
+
     let divd = tidebyminute / 25;
+
     let minute = divd * 15;
     
-    let Downhour = Uphour + 12;
+    let Downhour = Uphour + 6;
     
-    let BASE_API = {
-        getHour : Uphour,
-        getDownHour : Downhour,
-        getMinute : minute
+    class BASE_API  
+
+    {
+
+         Uphour;
+
+         Downhour;
+
+         minute;
     };
+
     console.log(BASE_API);
-    
-    // let BASE_API2 = `today is  ${getMmDay}th, Tide Up time is  ${Uphour}:${minute}PM and Tide Down time is ${Downhour}:${minute}PM`;
-    
-    // let BASE_API1 = `ဒီနေ့ကမြန်မာလို ${getMmDay} ရက်နေ့ဖြစ်ပြီး , ဒီ‌နေ့ ${Uphour}နာရီ${minute}မိနစ်မှာရေတက်ပါမည်။ရေကျမဲ့အချိန်ကတော့${Downhour}နာရီ${minute}မိနစ်ဖြစ်ပါတယ်။`;
     
    };
 
-Api();
+// Api();
 
+    
 
+    // }
 
-
+});
